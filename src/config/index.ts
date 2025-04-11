ee83aff36204b13e7585d496ed01eef46ba9d74e
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2025-04-11 15:47:53 -0400

    jwt fix

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,15 +2,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.JWT_SECRET) {
+// Solo validamos en desarrollo
+if (process.env.NODE_ENV !== 'production' && !process.env.JWT_SECRET) {
     throw new Error('La variable de entorno JWT_SECRET no está definida. Por favor, configúrala para continuar.');
-  }
+}
 
 export const config = {
   PORT: process.env.PORT || 3000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET || 'secret_for_development',
   ALLOWED_ORIGINS: process.env.ALLOWED_ORIGINS,
   CORS_ORIGIN: process.env.CORS_ORIGIN,
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
