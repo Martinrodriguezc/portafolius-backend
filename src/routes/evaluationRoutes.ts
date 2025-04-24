b1e843b40e8b629a897908482d5a50ae0f9e6823
brodper <brodper@uc.cl>
2025-04-24 16:32:46 -0400

    feat: implementación completa de diagnóstico por video

--- src/routes/evaluationRoutes.ts
+++ src/routes/evaluationRoutes.ts
@@ -1,14 +1,30 @@
 import { Router } from "express";
-import { createEvaluation } from "../controllers/evaluationController";
+import {
+  createEvaluation,
+  saveDiagnosis,
+  getDiagnosedVideos
+} from "../controllers/evaluationController/createEvaluation";
+
 import { authenticateToken } from "../middleware/authenticateToken";
 
 const router = Router();
 
 router.post(
-    "/:studyId",
-    (req, res, next) => { authenticateToken(req, res, next); },
-    createEvaluation
-  );
+  "/:studyId",
+  (req, res, next) => { authenticateToken(req, res, next); },
+  createEvaluation
+);
+
+router.post(
+  "/diagnosis/:videoId",
+  (req, res, next) => { authenticateToken(req, res, next); },
+  saveDiagnosis
+);
+
+router.get(
+  "/diagnosis",
+  (req, res, next) => { authenticateToken(req, res, next); },
+  getDiagnosedVideos
+);
 
 export default router;
-
