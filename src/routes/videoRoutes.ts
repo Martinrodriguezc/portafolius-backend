f41cb9f98106bf82d056ae756eb6d21c8270429d
tomasrodriguez1 <tomas.rodriguez1@uc.cl>
2025-04-23 23:11:33 -0400

    feat: add diagnosis seeds

--- src/routes/videoRoutes.ts
+++ src/routes/videoRoutes.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { generateUploadUrl } from "../controllers/videoController/uploadVideo";
 import { generateDownloadUrl } from "../controllers/videoController/downloadVideo";
 import { getVideoMetadata } from "../controllers/videoController/getVideoMetadata";
+import { getAllTags } from "../controllers/videoController/getTags";
 
 const router = Router();
 
 router.post("/generate_upload_url", generateUploadUrl);
 router.get("/generate_download_url/:clipId", generateDownloadUrl);
-router.get("/:id/meta", getVideoMetadata)
-
+router.get("/:id/meta", getVideoMetadata);
+router.get("/tags", getAllTags);
 
 export default router;
