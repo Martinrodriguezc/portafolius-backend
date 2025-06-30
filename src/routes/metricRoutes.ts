edb5f270063e96a737192d67d53fcd0dc0e172ab
MartinRamirez <martinramirez@uc.cl>
2025-06-30 12:46:46 -0400

    feat: statistic table student dashboard

--- src/routes/metricRoutes.ts
+++ src/routes/metricRoutes.ts
@@ -3,11 +3,13 @@ import { authenticateToken } from '../middleware/authenticateToken'
 import { getStudentDashboardMetrics } from '../controllers/metricsController/getStudentDashboardMetrics'
 import { getRecentComments } from '../controllers/studyController/getRecentComments'
 import { getStudentStats } from '../controllers/metricsController/getStudentStats'
+import { getInteractionMetrics } from '../controllers/metricsController/getInteractionMetrics';
 
 const router = Router()
 
 router.get('/:id/comments', getRecentComments)
 router.get('/:id/dashboard-metrics', authenticateToken, getStudentDashboardMetrics)
 router.get('/:id/student-stats', getStudentStats)
+router.get('/:studentId/interaction-metrics', authenticateToken, getInteractionMetrics)
 
 export default router
